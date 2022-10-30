c9c1fd61d30db5759a31ec36b3aff7fad8a085d8
jis22u <jis22u@gmail.com>
2022-10-31 01:25:27 +0900

    MVC

--- MVC/Spring_MyMVC/src/com/ssafy/mvc/controller/MyController.java
+++ MVC/Spring_MyMVC/src/com/ssafy/mvc/controller/MyController.java
@@ -26,7 +26,6 @@ public class MyController {
 
 	/**
 	 * DI와 IoC 적용
-	 * 
 	 * @param myService
 	 */
 	@Autowired
@@ -34,12 +33,12 @@ public class MyController {
 		this.myService = myService;
 	}
 
+	
 	/**
 	 * Controller가 Dispatcher Servlet에게 ModelAndView 객체를 전달
-	 * 
-	 * @RequestMapping()은 URL을 클래스 또는 특정 핸들러(메서드)에 매핑시킨다. 일반적으로 클래스에
-	 *                    작성하는 @RequestMapping은 요청 경로 혹은 요청 패턴에 매칭 메서드 Annotation은
-	 *                    요청방식(GET, POST) 등으로 범위를 좁혀 준다.
+	 * @RequestMapping()은 URL을 클래스 또는 특정 핸들러(메서드)에 매핑시킨다. 
+	 * 					일반적으로 클래스에 작성하는 @RequestMapping은 요청 경로 혹은 요청 패턴에 매칭 
+	 * 					메서드 Annotation은 요청방식(GET, POST) 등으로 범위를 좁혀 준다.
 	 * @return ModelAndView
 	 */
 	@RequestMapping("home")
@@ -60,6 +59,7 @@ public class MyController {
 		return mav;
 	}
 
+	
 	@RequestMapping("test11")
 	public String handler3(HttpServletRequest request, Locale locale, Model model) {
 		model.addAttribute("locale_name", locale.getDisplayName());
@@ -67,6 +67,16 @@ public class MyController {
 		return "test11";
 	}
 	
+	
+	/**
+	 * @PathVariable : URI 템플릿 변수에 대한 액세스.
+	 * 				 : 예를 들어, http://localhost:8080/pjt02/hello/test2/12?name=kang
+	 * 				 : 에서 '12'에 대한 엑세스
+	 * @param id
+	 * @param name
+	 * @param model
+	 * @return
+	 */
 	@RequestMapping("test22/{id}") 
 	public String handler4(@PathVariable int id, String name, Model model) {
 		model.addAttribute("path_var", id);
@@ -75,9 +85,10 @@ public class MyController {
 	}
 	
 	
-	
 	/**
-	 * required = true 설정을 해주면 요청 피라미터에 해당 속성이 없을 경우 400에러(잘못된 요청) 발생
+	 * @RequestParam : 예를 들어, http://localhost:8080/pjt02/hello/test2/12?name=kang
+	 * 				 : 에서 'name=kang'에 대한 엑세스
+	 * required : true 설정을 해주면 요청 피라미터에 해당 속성이 없을 경우 400에러(잘못된 요청) 발생
 	 * @param name
 	 * @param id
 	 * @param model
@@ -109,9 +120,7 @@ public class MyController {
 	}
 
 	
-	
 	/**
-	 * 
 	 * model 객체에 담아서 보냄
 	 * @return String
 	 */
@@ -121,11 +130,18 @@ public class MyController {
 		return "test2";
 	}
 
-	// Parmeter를 받고 싶다.
-	// 변수명이 파라미터의 키값이 된다.
-	// 파라미터의 값이 들어오지 않으면 null
-	// 기초자료형을 사용할 떄 Null이라고 하는 것은 말도 안되는 것
-
+	
+	/**
+	 * <Parmeter를 받는 경우>
+	 * 변수명이 파라미터의 키값이 된다.
+	 * 값이 들어오지 않으면 null이 되는데,
+	 *  1) String과 같은 참조자료형은 파라미터의 값이 들어오지 않으면 null. 
+	 * 	     이는 jstl 문법에 의해 예외가 뜨진 않고 빈칸으로 표시된다.
+	 *  2) 반면, 기초자료형이 null이라고 하는 것은 예외상황이다.(500에러 뜸)
+	 * 	   value = "age", defaultValue = "1"와 같이 null예외를 방지한다.
+	 * @RequestParam : multipart 파일을 포함하여 요청 파라미터에 엑세스
+	 * defaultValue : 요청 파라미터에 해당 속성이 없을 경우 기본값 지정
+	 */
 	@GetMapping("test3")
 	public String test3(Model model, String id, String pw, @RequestParam(value = "age", defaultValue = "1") int age) {
 		model.addAttribute("myid", id);
@@ -135,6 +151,7 @@ public class MyController {
 		return "test3";                       
 	}
 
+	
 	// 적절한 DTO를 준비를 해놓으니 Spring이 알아서 User 클래스의 필드에 대입
 	@PostMapping("test4")
 	public String test4(Model model, User user) {
@@ -144,14 +161,8 @@ public class MyController {
 	}
 	
 	
-	
-	
 	/**
-	 * 여기서의 "redirect"는 서블릿에서의 response.sendRedirect()와 다르다.
-	 * url 뒤에 'test8?message=test8'이 붙어 변경된다.
-	 * 이는 response.sendRedirect()처럼 url이 변경되는 것은 동일하지만
-	 * 기존 요청에 들어있는 정보들이 그대로 살아남은 채 페이지 이동이 된 것이다.
-	 * (그랬기 때문에 url 뒤에 쿼리 스트링이 붙어 있는 것)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
+	 * 여기서의 "redirect"는 서블릿에서의 response.sendRedirect()와 같다.
 	 * @param model
 	 * @return String
 	 */
@@ -166,7 +177,5 @@ public class MyController {
 	public String handler8() {
 		return "test9";
 	}
-	
-	
 
 }
