679d4939a1465dd271c0557b546a60b6d99e0468
jis22u <jis22u@gmail.com>
2023-01-24 01:38:04 +0900

    TIL Jpa 모든 연관관계 매핑 적용

---
+++ Jpa/jpashop_pjt/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,22 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Delivery {
+    @Id @GeneratedValue
+    private Long id;
+
+    private String city;
+    private String street;
+    private String zipcode;
+    private DeliveryStatus status;
+
+    // Order와 양방향 관계 매핑시
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+}
