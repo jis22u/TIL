7c250bbe53d2a063e057ded3ec474969a7a24eb9
jis22u <jis22u@gmail.com>
2023-05-02 14:35:05 +0900

    Vue3 커스텀 api, 환경변수 설정

---
+++ vue3-posts/src/api/index.js
@@ -0,0 +1,12 @@
+﻿import axios from 'axios';
+
+function create(baseURL, options) {
+	const instance = axios.create(Object.assign({ baseURL }, options));
+	return instance;
+}
+
+export const posts = create(`${import.meta.env.VITE_APP_API_URL}posts/`);
+// development: http://localhost:5000/posts/   (개발 주소)
+// produciton: http://localhost:5001/posts/    (운영 주소)
+
+// export const comments = create('http://localhost:5000/comments/');
--- vue3-posts/src/router/index.js
+++ vue3-posts/src/router/index.js
@@ -39,10 +39,10 @@ const routes = [
 		path: '/posts/:id',
 		name: 'PostDetail',
 		component: PostDetailView,
-		// props: true,
-		props: route => ({
-			id: parseInt(route.params.id),
-		}),
+		props: true,
+		// props: route => ({
+		// 	id: parseInt(route.params.id),
+		// }),
 	},
 	{
 		path: '/posts/:id/edit',
